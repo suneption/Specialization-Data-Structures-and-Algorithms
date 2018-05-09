c3633db66ebc42d0dba4cf262cf847c8f7de0b6b
Unknown <spayn747@mail.ru>
2018-05-09 20:12:51 +0300

    Graphs

---
+++ Algorithms on Graphs/Graphs.W1.Decomposition1.Reachability/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.W1.Decomposition1.Reachability
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nm = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var n = int.Parse(nm[0]);
+            var m = int.Parse(nm[1]);
+
+            var scheme = Enumerable.Range(0, m)
+                .Select(x => 
+                    Console.ReadLine()
+                        .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(y => int.Parse(y)).ToList())
+                .ToList();
+
+            var startEnd = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            var start = int.Parse(startEnd[0]);
+            var end = int.Parse(startEnd[1]);
+
+            var result = Path(scheme, start, end);
+
+            Console.WriteLine(result);
+        }
+
+        public static int Path(List<List<int>> scheme, int start, int end)
+        {
+            var nodes = new Dictionary<int, Node>();
+            foreach (var edge in scheme)
+            {
+                var source = edge[0];
+                var target = edge[1];
+
+                Node sourceNode = null;
+                if (!nodes.TryGetValue(source, out sourceNode))
+                {
+                    sourceNode = new Node { Value = source, Refs = new List<Node>() };
+                    nodes.Add(source, sourceNode);
+                }
+
+                Node targetNode = null;
+                if (!nodes.TryGetValue(target, out targetNode))
+                {
+                    targetNode = new Node { Value = target, Refs = new List<Node>() };
+                    nodes.Add(target, targetNode);
+                }
+
+                Contract.Assert(!sourceNode.Refs.Any(x => x.Value == targetNode.Value));
+                sourceNode.Refs.Add(targetNode);
+
+                Contract.Assert(!targetNode.Refs.Any(x => x.Value == sourceNode.Value));
+                targetNode.Refs.Add(sourceNode);
+            }
+
+            var result = 0;
+            if (!nodes.ContainsKey(start))
+            {
+                return result;
+            }
+
+            var startNode = nodes[start];
+            startNode.IsVisited = true;
+            var nextNodes = new Stack<Node>();
+            nextNodes.Push(startNode);
+            while (nextNodes.Any())
+            {
+                var current = nextNodes.Pop();
+
+                if (current.Value == end)
+                {
+                    result = 1;
+                    break;
+                }
+
+                current.IsVisited = true;
+
+                var notVisited = current.Refs.Where(x => !x.IsVisited).ToList();
+                notVisited.ForEach(x => nextNodes.Push(x));
+            }
+
+            return result;
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public List<Node> Refs { get; set; }
+        public bool IsVisited { get; set; }
+    }
+}
---
+++ Algorithms on Graphs/Graphs.W1.Decomposition1.Reachability/Tests.cs
@@ -0,0 +1,69 @@
+﻿using Graphs.W1.Decomposition1.Reachability.Properties;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.W1.Decomposition1.Reachability
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Reachability_TestCase1_Results1()
+        {
+            var testCase = Resources._1;
+            var resultStr = new StringBuilder();
+            using (var input = new StringReader(testCase))
+            using (var output = new StringWriter(resultStr))
+            {
+                Console.SetIn(input);
+                Console.SetOut(output);
+
+                Program.Main(new string[] { });
+
+                var result = resultStr.ToString();
+                Assert.That(result.Trim() == "1");
+            }
+        }
+
+        [Test]
+        public void Reachability_TestCase2_Results0()
+        {
+            var testCase = Resources._2;
+            var resultStr = new StringBuilder();
+            using (var input = new StringReader(testCase))
+            using (var output = new StringWriter(resultStr))
+            {
+                Console.SetIn(input);
+                Console.SetOut(output);
+
+                Program.Main(new string[] { });
+
+                var result = resultStr.ToString();
+                Assert.That(result.Trim() == "0");
+            }
+        }
+
+        [Test]
+        public void Reachability_TestCase3_Results0()
+        {
+            var testCase = Resources._3;
+            var resultStr = new StringBuilder();
+            using (var input = new StringReader(testCase))
+            using (var output = new StringWriter(resultStr))
+            {
+                Console.SetIn(input);
+                Console.SetOut(output);
+
+                Program.Main(new string[] { });
+
+                var result = resultStr.ToString();
+                Assert.That(result.Trim() == "0");
+            }
+        }
+    }
+}
